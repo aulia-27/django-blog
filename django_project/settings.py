2f66e7f88e84370f86a03f304c0c0bcec9b102f6
Muhammad Aulia Rahman <aulia027.aa@gmail.com>
2021-11-19 22:46:52 +0700

    login, logout, proses, crud

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'blog',
     'users',
     'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'blog-home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
